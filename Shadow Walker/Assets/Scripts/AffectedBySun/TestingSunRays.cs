de47ce4a8fd933ad300678b7970aae684572ac79
Migge <miggex91@hotmail.com>
2019-04-11 21:23:16 +0200

    First Sun Test
    Created a temp player
    Created a player controller
    Created a temp sun
    Created temporary background
    Created temporary obstacles

---
+++ Shadow Walker/Assets/Scripts/AffectedBySun/TestingSunRays.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingSunRays : MonoBehaviour
+{
+
+    private GameObject sun;
+    [SerializeField]
+    private LayerMask obstacleLayer;
+
+    private PolygonCollider2D col;
+    private Vector2[] colPoints;
+    public bool isExposedToSunlight;
+
+    void Start()
+    {
+        col = GetComponent<PolygonCollider2D>();
+        colPoints = col.points;
+        sun = GameObject.Find("Sun");
+        isExposedToSunlight = false;
+    }
+
+    void Update()
+    {
+
+        for(int i = 0; i < colPoints.Length; i++ )
+        {
+            //Vector3 polygonPoint = new Vector3(transform.position.x + colPoints[i].x, transform.position.y + colPoints[i].y);
+            Vector3 polygonPoint = transform.TransformPoint(colPoints[i]);
+            Vector2 DirToSun = (sun.transform.position - polygonPoint).normalized;
+            float DistanceToSun = Vector2.Distance(transform.position, sun.transform.position);
+
+            if(!Physics2D.Raycast(polygonPoint, DirToSun,DistanceToSun,obstacleLayer))
+            {
+                isExposedToSunlight = true;
+                Debug.DrawLine(polygonPoint, sun.transform.position, Color.red);
+            }
+            else
+            {
+                isExposedToSunlight = false;
+                Debug.DrawLine(polygonPoint, sun.transform.position, Color.blue);
+            }
+        }
+    }
+}
---
+++ Shadow Walker/Assets/Scripts/PlayerMovement/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float jumpForce = 5f;
+    [SerializeField]
+    private float groundCheckRadius = 0.2f;
+    [SerializeField]
+    private float jumpingTimer = 0.4f;
+    [SerializeField]
+    private bool onGround = false;
+    [SerializeField]
+    private Transform groundChecker = null;
+    [SerializeField]
+    private LayerMask ground = 0;
+
+
+    private Rigidbody2D rb;
+    private float movement = 0f;
+    private float jumpingCounter = 0f;
+    private bool facingRight = true;
+    private bool jumping = false;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        facingRight = true;
+    }
+
+    void Update()
+    {
+        if (onGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            onGround = false;
+            jumping = true;
+            jumpingCounter = jumpingTimer;
+            //rb.AddForce(new Vector2(0f, jumpForce));
+            rb.velocity = Vector2.up * jumpForce;
+        }
+        if (Input.GetKey(KeyCode.Space) && jumping == true)
+        {
+            if (jumpingCounter >= 0)
+            {
+                //rb.AddForce(new Vector2(0f, jumpForce));
+                rb.velocity = Vector2.up * jumpForce;
+                jumpingCounter -= Time.deltaTime;
+            }
+            else
+            {
+                jumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            jumping = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        movement = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(movement * speed, rb.velocity.y);
+
+        if (facingRight == false && movement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && movement < 0)
+        {
+            Flip();
+        }
+
+        onGround = Physics2D.OverlapCircle(groundChecker.position, groundCheckRadius, ground);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 characterScale = transform.localScale;
+        characterScale.x *= -1;
+        transform.localScale = characterScale;
+    }
+}
