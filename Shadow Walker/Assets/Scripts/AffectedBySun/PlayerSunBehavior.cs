0e5dbd68161e14edc28b4aee017fd72e4c6357fb
Migge <miggex91@hotmail.com>
2019-04-12 16:32:45 +0200

    Defined Player Behavior When Affected by the sun
    Added some basic behaviors, removed it from the testScript

---
+++ Shadow Walker/Assets/Scripts/AffectedBySun/PlayerSunBehavior.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSunBehavior : AffectedByTheSun
+{
+    private GameObject startingPoint;
+
+    private PlayerController playerController;
+
+
+    public void Start()
+    {
+        AffectedByTheSunScriptStart();
+
+        playerController = GetComponent<PlayerController>();
+        startingPoint = GameObject.Find("StartingPoint");
+    }
+
+    public void Update()
+    {
+        AffectedByTheSunScriptUpdate();
+    }
+
+    public override void JustGotCoveredFromSunlight()
+    {
+        Debug.Log("JustGotCoveredFromSunlight()");
+    }
+
+    public override void JustGotExposedToSunlight()
+    {
+        playerController.StopAllMovement(0.8f);
+        transform.position = startingPoint.transform.position;
+        Debug.Log("JustGotExposedToSunlight()");
+
+    }
+
+    public override void UnderFullCover()
+    {
+        Debug.Log("UnderFullCover()");
+    }
+
+    public override void UnderFullExposure()
+    {
+        Debug.Log("UnderFullExposure()");
+    }
+
+    public override void UnderPartialCover()
+    {
+        Debug.Log("UnderPartialCover()");
+    }
+
+}
