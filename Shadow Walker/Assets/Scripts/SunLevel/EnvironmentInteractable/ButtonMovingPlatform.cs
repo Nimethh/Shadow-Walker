0615e0cb4a3dd48c4d79a707c68c86c8b21588ae
Migge <miggex91@hotmail.com>
2019-04-15 19:02:05 +0200

    Created a moving platform
    Created a button that controls a moving platform

---
+++ Shadow Walker/Assets/Scripts/SunLevel/EnvironmentInteractable/ButtonMovingPlatform.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonMovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform movingPlatform;
+    private Rigidbody2D movingPlatformRigid;
+
+    [SerializeField]
+    private float movingSpeed;
+
+    Vector3 direction;
+    Transform movingDestination;
+
+
+    //Visualisation Aid ( Only for the scene window )
+    [SerializeField]
+    private Transform startTransform;
+    [SerializeField]
+    private Transform endTransform;
+
+    void Start()
+    {
+        movingPlatformRigid = movingPlatform.GetComponent<Rigidbody2D>();
+        SetDestination(startTransform);
+    }
+
+    void FixedUpdate()
+    {
+        movingPlatformRigid.MovePosition(movingPlatform.position + direction * movingSpeed * Time.fixedDeltaTime);
+
+        if (Vector3.Distance(movingPlatform.position, movingDestination.position) < movingSpeed * Time.fixedDeltaTime)
+        {
+            SetDestination(movingDestination == startTransform ? endTransform : startTransform);
+        }
+    }
+
+    void SetDestination(Transform destination)
+    {
+        movingDestination = destination;
+        direction = (movingDestination.position - movingPlatform.position).normalized;
+    }
+
+    private void OnDrawGizmos() //Visualisation Aid
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(startTransform.position, new Vector3(0.2f,0.2f,0.1f));
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(endTransform.position, new Vector3(0.2f, 0.2f, 0.1f));
+    }
+}
---
+++ Shadow Walker/Assets/Scripts/SunMoonMovement/SunController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] points;
+
+    private int numberOfPoints = 100;
+
+    private int index = 0;
+
+    [SerializeField]
+    private Transform centerPosition;
+
+    private void Start()
+    {
+        //lineRenderer.positionCount = numberOfPoints;
+        transform.position = points[0].position;
+        //DrawQuadraticCurve();
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.L) && transform.position.x < points[2].position.x)
+        {
+            MoveRight();
+        }
+        if (Input.GetKey(KeyCode.J) && transform.position.x > points[0].position.x)
+        {
+            MoveLeft();
+        }
+
+        //Rotate();
+
+    }
+
+    void MoveRight()
+    {
+        index++;
+        float t = index / (float)numberOfPoints * 0.5f;
+        transform.position = CalculateQuadraticBezeirPoint(t, points[0].position, points[1].position, points[2].position);
+    }
+
+    void MoveLeft()
+    {
+        index--;
+        float t = index / (float)numberOfPoints * 0.5f;
+        transform.position = CalculateQuadraticBezeirPoint(t, points[0].position, points[1].position, points[2].position);
+    }
+
+    void Rotate()
+    {
+        transform.LookAt(centerPosition);
+    }
+
+    //void DrawQuadraticCurve()
+    //{
+    //    for (int i = 1; i < numberOfPoints + 1; i++)
+    //    {
+    //        float t = i / (float)numberOfPoints;
+    //        positions[i - 1] = CalculateQuadraticBezeirPoint(t, points[0].position, points[1].position, points[2].position);
+    //    }
+    //    lineRenderer.SetPositions(positions);
+    //}
+
+    Vector3 CalculateQuadraticBezeirPoint(float t, Vector3 point1, Vector3 point2, Vector3 point3)
+    {
+        float i = 1 - t;
+        float tSquared = t * t;
+        float iSquared = i * i;
+
+        Vector3 point = (iSquared * point1) + (2 * i * t * point2) + (tSquared * point3);
+
+        return point;
+    }
+
+}
