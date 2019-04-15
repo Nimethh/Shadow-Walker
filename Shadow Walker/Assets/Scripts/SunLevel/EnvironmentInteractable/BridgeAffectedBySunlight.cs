3e82599aa7de7e408622c4a26c32c7fa8872517b
Migge <miggex91@hotmail.com>
2019-04-15 19:01:22 +0200

    Created a bridge that reacts to the sunlight

---
+++ Shadow Walker/Assets/Scripts/SunLevel/EnvironmentInteractable/BridgeAffectedBySunlight.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeAffectedBySunlight : AffectedByTheSun
+{
+    public GameObject bridgeObject;
+    public bool bridgeActive;
+
+    void Start()
+    {
+        AffectedByTheSunScriptStart();
+    }
+
+    void Update()
+    {
+        AffectedByTheSunScriptUpdate();
+    }
+
+
+    public override void JustGotCoveredFromSunlight()
+    {
+        bridgeActive = true;
+        bridgeObject.SetActive(true);
+        Debug.Log("Button JustGotCoveredFromSunlight()");
+    }
+
+    public override void JustGotExposedToSunlight()
+    {
+        bridgeActive = false;
+        bridgeObject.SetActive(false);
+        Debug.Log("Button JustGotExposedToSunlight()");
+    }
+
+    public override void UnderFullCover()
+    {
+        Debug.Log("Button UnderFullCover()");
+    }
+
+    public override void UnderFullExposure()
+    {
+        Debug.Log("Button UnderFullExposure()");
+    }
+
+    public override void UnderPartialCover()
+    {
+        Debug.Log("Button UnderPartialCover()");
+    }
+
+}
