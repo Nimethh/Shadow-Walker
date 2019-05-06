3cb623a1cc8957a82d5fe1b1838c6071f9f470c8
Karam Hariri <karam.hariri@outlook.com>
2019-05-06 17:52:02 +0200

    Added Walking Animation for the player

--- Shadow Walker/Assets/Scripts/MoonLevel/PlayerController/PlayerInput.cs
+++ Shadow Walker/Assets/Scripts/MoonLevel/PlayerController/PlayerInput.cs
@@ -8,11 +8,15 @@ public class PlayerInput : MonoBehaviour
 	Player player;
     Vector2 directionalInput;
     Controller2D controller;
+    Animator animator;
+    private bool facingRight = true;
+    private SpriteRenderer spriteRenderer;
 
     void Start ()
     {
 		player = GetComponent<Player> ();
         controller = GetComponent<Controller2D>();
+        animator = GetComponent<Animator>();
 	}
 
 	void Update ()
@@ -21,14 +25,50 @@ public class PlayerInput : MonoBehaviour
         if (controller.collisionInfo.climbing == false || controller.collisionInfo.below == true)
         {
             directionalInput.x = Input.GetAxisRaw("Horizontal");
+            Debug.Log(directionalInput.x);
+            if (directionalInput.x > 0)
+            {
+                animator.SetBool("WalkRight", true);
+                animator.SetBool("WalkLeft", false);
+                animator.SetBool("Idle", false);
+            }
+            else if(directionalInput.x < 0)
+            {
+                animator.SetBool("WalkLeft", true);
+                animator.SetBool("WalkRight", false);
+                animator.SetBool("Idle", false);
+            }
+            else
+            {
+                animator.SetBool("Idle", true);
+                animator.SetBool("WalkRight", false);
+                animator.SetBool("WalkLeft", false);
+            }
         }
         directionalInput.y = Input.GetAxisRaw("Vertical");
         player.SetDirectionalInput (directionalInput);
 
-		if (Input.GetKeyDown (KeyCode.Space))
+        if (facingRight == false && directionalInput.x > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && directionalInput.x < 0)
+        {
+            Flip();
+        }
+
+        if (Input.GetKeyDown (KeyCode.Space))
         {
 			player.OnJumpInputDown ();
             FindObjectOfType<AudioManager>().Play("Jump");
         }
-	}
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 characterScale = transform.localScale;
+        characterScale.x *= -1;
+        transform.localScale = characterScale;
+    }
 }
