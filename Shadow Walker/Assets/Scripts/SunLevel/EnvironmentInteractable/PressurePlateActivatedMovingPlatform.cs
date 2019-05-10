9c29d137cb3f0ec64cbdc2f63b171cbfe8d810b2
Migge <miggex91@hotmail.com>
2019-05-10 12:41:37 +0200

    Added missing components from previous commit

---
+++ Shadow Walker/Assets/Scripts/SunLevel/EnvironmentInteractable/PressurePlateActivatedMovingPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateActivatedMovingPlatform : MonoBehaviour
+{
+    public MovingPlatformWithEasingAndShake platform;
+
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite plate;
+    [SerializeField] private Sprite plateActivated;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Box"))
+        {
+            platform.activated = true;
+
+            spriteRenderer.sprite = plateActivated;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Box"))
+        {
+            platform.activated = false;
+            spriteRenderer.sprite = plate;
+
+        }
+    }
+}
