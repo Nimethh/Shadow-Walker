667c4e7d742889acea93e5ba8b37a7fd57988993
Karam <Karam.hariri@outlook.com>
2019-11-11 22:01:26 +0100

    Sun drag controller

--- Shadow Walker/Assets/Scripts/MobileScripts/Sun/SunControllerMobile.cs
+++ Shadow Walker/Assets/Scripts/MobileScripts/Sun/SunControllerMobile.cs
@@ -8,7 +8,7 @@ public class SunControllerMobile : MonoBehaviour
     private Transform[] points;
 
     //public int checkPointIndex = 100;
-    public int index;
+    public float index;
     private int numberOfPoints = 100;
 
     float startT;
@@ -20,10 +20,23 @@ public class SunControllerMobile : MonoBehaviour
     [SerializeField]
     private float maxVelocity = 0.7f;
 
+    //float timerStart = 0.0f;
+    //float currentTimer = 0.0f;
+
+    float currentPositionPercentage = 0.0f;
+
     [SerializeField]
     private bool invertTiltController = false;
 
     AudioManager audioManager;
+
+    public enum SunMovementType
+    {
+        DRAG,
+        TILT
+    }
+
+    public SunMovementType movementType;
 
     //public VirtualSunJoystick sunJoystick;
 
@@ -31,6 +44,7 @@ public class SunControllerMobile : MonoBehaviour
     {
         startT = index / (float)numberOfPoints * sunSpeed;
         transform.position = CalculateQuadraticBezeirPoint(startT, points[0].position, points[1].position, points[2].position);
+        currentPositionPercentage = startT;
         audioManager = FindObjectOfType<AudioManager>();
         velocity = 0.0f; 
     }
@@ -55,10 +69,14 @@ public class SunControllerMobile : MonoBehaviour
         {
             MoveLeft();
         }*/
-
+        if(movementType == SunMovementType.DRAG)
+        {
+            return;
+        }
+         
         velocity = Input.acceleration.x;
 
-        if(invertTiltController == true)
+        if(invertTiltController == false)
         {
             if (velocity < -0.15f && transform.position.x > points[0].position.x)
             {
@@ -73,7 +91,7 @@ public class SunControllerMobile : MonoBehaviour
                 audioManager.Mute("SunMoving");
             }
         }
-        else if(invertTiltController == false)
+        else if(invertTiltController == true)
         {
             if(velocity < -0.15f && transform.position.x < points[2].position.x)
             {
@@ -92,7 +110,6 @@ public class SunControllerMobile : MonoBehaviour
 
     void MoveRight()
     {
-
         velocity = Mathf.Clamp(velocity, -maxVelocity, maxVelocity);
         t = t + (velocity * velocity) * 0.02f;
         transform.position = CalculateQuadraticBezeirPoint(t, points[0].position, points[1].position, points[2].position);
@@ -105,6 +122,16 @@ public class SunControllerMobile : MonoBehaviour
         t = t - (velocity * velocity) * 0.02f;
         transform.position = CalculateQuadraticBezeirPoint(t, points[0].position, points[1].position, points[2].position);
         audioManager.Play("SunMoving");
+    }
+
+    public void UpdateDragMovement(float t)
+    {
+        if (Mathf.Abs(currentPositionPercentage - t) <= 0.1f)
+        {
+            t = Mathf.Clamp01(t);
+            transform.position = CalculateQuadraticBezeirPoint(t, points[0].position, points[1].position, points[2].position);
+            currentPositionPercentage = t;
+        }
     }
 
     Vector3 CalculateQuadraticBezeirPoint(float t, Vector3 point1, Vector3 point2, Vector3 point3)
---
+++ Shadow Walker/Assets/Scripts/MobileScripts/Sun/SunDragControllerMobile.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SunDragControllerMobile : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
+{
+
+    Canvas canvas;
+
+    [SerializeField]
+    private Image sunSpace;
+
+    [SerializeField]
+    private SunControllerMobile sun;
+
+    private bool allowingMovement = false;
+
+    void Start()
+    {
+        canvas = transform.GetComponentInParent<Canvas>();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnDrag(PointerEventData ped)
+    {
+        if (sun.movementType != SunControllerMobile.SunMovementType.DRAG)
+            return;
+
+        Vector2 pos = Vector2.zero;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(sunSpace.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            float size = canvas.scaleFactor * 1920f;
+            float halfSize = size / 2f;
+            float sunPositionPercent = ((pos.x + halfSize) / (size));
+            Debug.Log("OnDrag! scaled position = " + ((pos.x + halfSize) / (size)));
+            sun.UpdateDragMovement(sunPositionPercent);
+        }
+    }
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+        if (sun.movementType != SunControllerMobile.SunMovementType.DRAG)
+            return;
+
+        Vector2 pos = Vector2.zero;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(sunSpace.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            //allowingMovement = true;
+            Debug.Log("Allowing movement");
+        }
+    }
+
+    public void OnPointerUp(PointerEventData ped)
+    {
+        if (sun.movementType != SunControllerMobile.SunMovementType.DRAG)
+            return;
+
+        Vector2 pos = Vector2.zero;
+
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(sunSpace.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        {
+            //allowingMovement = false;
+            Debug.Log("Not allowing movement");
+        }
+    }
+
+}
